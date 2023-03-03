59e8fe2ebda67a47894763232970883fd78d6c01
eshhusv <ak7527203@gmail.com>
2023-03-03 20:53:58 +0200

    Add project files.

---
+++ fivedotone/Program.cs
@@ -0,0 +1,8 @@
+﻿Random r = new Random();
+char[] a = new char[10];
+for(int i = 0; i < a.Length; i ++)
+{
+    a[i] = (char)r.Next(0x0410, 0x44F);
+    if (a[i] == '0' || a[i] == '1' || a[i] == '2' || a[i] == '3' || a[i] == '4' || a[i] == '5' || a[i] == '6' || a[i] == '7' || a[i] == '8' || a[i] == '9') a[i] = '*';
+    Console.Write(a[i]);
+}
